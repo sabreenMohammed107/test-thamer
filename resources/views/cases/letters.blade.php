d57b9b77799fc4b3bd5397478c0c919d3ab6590c
sabreenMohammed107 <sabreenm312@gmail.com>
2021-12-26 09:44:54 +0200

    hi

--- resources/views/cases/letters.blade.php
+++ resources/views/cases/letters.blade.php
@@ -30,7 +30,7 @@
             <th>{{$row->notes}}</th>
             <th>
                 <div class="btn-group">
-                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#done2{{ $row->id }}"><i class="fas fa-check" title="view"></i></button>
+                    <button type="button"  @if($task->task_status_id==1) disabled @endif class="btn btn-default" data-toggle="modal" data-target="#done2{{ $row->id }}"><i class="fas fa-check" title="view"></i></button>
                 </div>
             </th>
             <th>
--- resources/views/cases/regulations.blade.php
+++ resources/views/cases/regulations.blade.php
@@ -34,7 +34,7 @@
             <th>@if($task->task_status_id==1) منجزة @else غير منجزة @endif</th>
             <th>
                 <div class="btn-group">
-                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#done3{{ $row->id }}"><i class="fas fa-check" title="view"></i></button>
+                    <button type="button"  @if($task->task_status_id==1) disabled @endif class="btn btn-default" data-toggle="modal" data-target="#done3{{ $row->id }}"><i class="fas fa-check" title="view"></i></button>
                 </div>
             </th>
             <th>
