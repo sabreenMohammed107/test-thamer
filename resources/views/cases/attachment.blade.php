10ce7ceabb3c13c8bb1b66b78b7ee5204aa62c31
sabreenMohammed107 <sabreenm312@gmail.com>
2021-12-26 09:59:28 +0200

    hi

--- resources/views/cases/attachment.blade.php
+++ resources/views/cases/attachment.blade.php
@@ -30,7 +30,7 @@
                 <div class="btn-group">
                     <button type="button" class="btn btn-default" data-toggle="modal" data-target="#view-tab7{{$row->id}}"><i class="fas fa-eye" title="view"></i></button>
                     <button type="button" class="btn btn-default" data-toggle="modal" data-target="#edit-tab7{{$row->id}}"><i class="fas fa-edit" title="edit"></i></button>
-                    <button type="button" class="btn btn-default"><i class="fas fa-print" title="print"></i></button>
+                    {{-- <button type="button" class="btn btn-default"><i class="fas fa-print" title="print"></i></button> --}}
                     @can('cases-delete')
                     <button type="button" class="btn btn-default" data-toggle="modal"
                     data-target="#del7{{ $row->id }}"><i class="fas fa-trash-alt"></i></button>
